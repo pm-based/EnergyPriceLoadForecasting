a01cfc71b8433509d769ed5df2fe8d02c95191d9
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2024-05-11 16:41:58 +0200

    Implement optuna for ARX models

--- tools/models/ARX.py
+++ tools/models/ARX.py
@@ -120,19 +120,22 @@ class ARXRegressor:
 
     @staticmethod
     def get_hyperparams_trial(trial, settings):
-        settings['l1'] = trial.suggest_float('l1', 1e-7, 1e-1)
+        settings['l1'] = trial.suggest_float('l1', 1e-7, 1e-1, log=True)
+        settings['l2'] = trial.suggest_float('l2', 1e-7, 1e-1, log=True)
         settings['lr'] = trial.suggest_float('lr', 1e-5, 1e-1, log=True)
         return settings
 
     @staticmethod
     def get_hyperparams_searchspace():
-        return {'l1': [1e-7, 1e-6, 1e-5, 1e-4, 1e-3],
+        return {'l1': [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
+                'l2': [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
                 'lr': [1e-4, 1e-3, 1e-2]}
 
     @staticmethod
     def get_hyperparams_dict_from_configs(configs):
         model_hyperparams = {
             'l1': configs['l1'],
+            'l2': configs['l2'],
             'lr': configs['lr']
         }
         return model_hyperparams
